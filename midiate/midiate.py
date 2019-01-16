a8e526237fb645e4ebb2c9edd0301936b12d357a
PGkids Laboratory <46478949+PGkids@users.noreply.github.com>
2019-01-16 18:39:03 +0900

    dev

--- midiate/midiate.py
+++ midiate/midiate.py
@@ -1,27 +1,20 @@
 # -*- coding: utf-8 -*-
+
 import sys
 import subprocess
 import threading
 import time
+import pkg_resources
 
-proc = None
-crlf = None
-endmsg = None
-terminator = None
-thread_monitor_stdout = None
-thread_monitor_stderr = None
-sem_1 = None
-sem_2 = None
-gResult = None
-gError = None
-callback_dic = None
-
-def getdir():
-    return __name__
-
-def trunc(str):
-    return str[0:(-2 if crlf else -1)]
-
+def get_default_bin_path():
+    path = pkg_resources.resource_filename(__name__, 'win32/intermidiator.exe')
+    if path.lower().find('a:\\dropbox\\home\\git\\pymidiate\\')==0:
+        # PGkids Laboratory Internal Environment
+        print('---------- DEVELOP ----------')
+        return 'a:/Dropbox/home/git/interMidiator/intermidiator.exe'
+    else:
+        # Public Environment
+        return path
 
 def chr2hex(ascii):
     if ascii <= 57: return (ascii-48)
@@ -36,9 +29,6 @@ def int_from_hex2(c1,c2):
 def take_handle_from_recv_msg(line):
     return int_from_hex3(line[2],line[3],line[4])
 
-prev_dev = None
-prev_msg = None
-prev_raw_msg = None
 
 def decode_callback_id(line, i):
     result = 0
@@ -57,187 +47,199 @@ def decode_to_raw(b):
         r2 = int_from_hex2(b[2],b[3])
         if length == 4: return (r1,r2)
         else: return (r1,r2,int_from_hex2(b[4],b[5]))
-        
+
+class Midiator():
+    def __init__(self,interMidiator=get_default_bin_path()):
+        self.interMidiator = interMidiator
+        self.proc = None
+        self.crlf = None
+        self.endmsg = None
+        self.terminator = None
+        self.thread_monitor_stdout = None
+        self.thread_monitor_stderr = None
+        self.sem_1 = None
+        self.sem_2 = None
+        self.gResult = None
+        self.gError = None
+        self.callback_dic = None
+        self.prev_dev = None
+        self.prev_msg = None
+        self.prev_raw_msg = None
+
+    def trunc(self,str):
+        return str[0:(-2 if self.crlf else -1)]
+
+    def monitor_stdout(self):
+        while True:
+            line = self.proc.stdout.readline()
+            if line==b'': return
+            cmd = line[0];
+            if cmd == ord('C'):
+                id = decode_callback_id(line,2)
+                print('the id=',id)
+                function = self.callback_dic[id]
+                if self.prev_raw_msg is None: self.prev_raw_msg = decode_to_raw(self.prev_msg)
+                function(self.prev_dev, self.prev_msg, self.prev_raw_msg)
+            elif   cmd == ord('1'):
+                self.prev_dev = take_handle_from_recv_msg(line)
+                self.prev_msg = line[6:8]
+                self.prev_raw_msg = None
+            elif cmd == ord('2'): 
+                self.prev_dev = take_handle_from_recv_msg(line)
+                self.prev_msg = line[6:10]
+                self.prev_raw_msg = None
+            elif cmd == ord('3'):
+                self.prev_dev = take_handle_from_recv_msg(line)
+                self.prev_msg = line[6:12]
+                self.prev_raw_msg = None
+            elif cmd == ord('{'):
+                self.gResult = []
+                while True:
+                    line = self.proc.stdout.readline()
+                    if line != (b'}\r\n' if self.crlf else b'}\n'):
+                        self.gResult.append(self.trunc(line).decode())
+                    else: break
+                self.sem_1.release()
+                self.sem_2.acquire()
+            elif cmd == ord('<'):
+                self.gResult = self.trunc(line[2:])
+                self.sem_1.release()
+                self.sem_2.acquire()
+            elif cmd == ord('!') or cmd == ord('F'):
+                self.gResult = None
+                self.gError = self.trunc(line[2:])
+                #sem_1.release()
+                #sem_2.acquire()
+
+            #else:        
+            print("[OUT]",line)
+            sys.stdout.flush();
+
+    def monitor_stderr(self):
+        while True:
+            line = self.proc.stderr.readline()
+            if line==b'': return
+            print("[ERR]",line)
+            sys.stdout.flush();
+
+    def _prepare(self):
+        self.proc = subprocess.Popen(self.interMidiator, #'a:/dropbox/home/git/intermidiator/intermidiator.exe',
+                                stdin=subprocess.PIPE,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+        s_ready = self.proc.stdout.readline();
+        if s_ready == b'READY\r\n' : self.crlf, self.terminator = True, b'\r\n'
+        elif s_ready == b'READY\n' : self.crlf, self.terminator = False, b'\n'
+        else:
+            print('ERROR!')
+            return False
+
+        #monitor_stdout, monitor_stderr = self.generate_monitors();
+        self.thread_monitor_stdout = threading.Thread(target=self.monitor_stdout)
+        self.thread_monitor_stderr = threading.Thread(target=self.monitor_stderr)
+        self.sem_1 = threading.Semaphore(0)
+        self.sem_2 = threading.Semaphore(0)
+        self.callback_dic = {}
+        return True
+
+    def _start(self):
+        self.thread_monitor_stdout.start()
+        self.thread_monitor_stderr.start()
+
+    def start_process(self):
+        self._prepare()
+        self._start()
 
 
-def monitor_stdout():
-    global prev_dev, prev_msg, prev_raw_msg
-    while True:
-        line = proc.stdout.readline()
-        if line==b'': return
-        cmd = line[0];
-        if cmd == ord('C'):
-            id = decode_callback_id(line,2)
-            print('the id=',id)
-            function = callback_dic[id]
-            if prev_raw_msg is None: prev_raw_msg = decode_to_raw(prev_msg)
-            function(prev_dev, prev_msg, prev_raw_msg)
-        elif   cmd == ord('1'):
-            prev_dev = take_handle_from_recv_msg(line)
-            prev_msg = line[6:8]
-            prev_raw_msg = None
-        elif cmd == ord('2'): 
-            prev_dev = take_handle_from_recv_msg(line)
-            prev_msg = line[6:10]
-            prev_raw_msg = None
-        elif cmd == ord('3'):
-            prev_dev = take_handle_from_recv_msg(line)
-            prev_msg = line[6:12]
-            prev_raw_msg = None
-        elif cmd == ord('{'):
-            global gResult
-            gResult = []
-            while True:
-                line = proc.stdout.readline()
-                if line != (b'}\r\n' if crlf else b'}\n'):
-                    gResult.append(trunc(line).decode())
-                else: break
-            sem_1.release()
-            sem_2.acquire()
-        elif cmd == ord('<'):
-            gResult = trunc(line[2:])
-            sem_1.release()
-            sem_2.acquire()
-        elif cmd == ord('!') or cmd == ord('F'):
-            gResult = None
-            gError = trunc(line[2:])
-            #sem_1.release()
-            #sem_2.acquire()
+    def _enum_io(self,cmd):
+        self.proc.stdin.write(cmd)
+        self.proc.stdin.write(self.terminator)
+        self.proc.stdin.flush()
+        self.sem_1.acquire()
+        names = self.gResult;
+        self.sem_2.release()
+        return names
 
-        #else:        
-        print("[OUT]",line)
-        sys.stdout.flush();
+    def enum_input(self):
+        return self._enum_io(b'LIST INPUT')
+    def enum_output(self):
+        return self._enum_io(b'LIST OUTPUT')
 
-def monitor_stderr():
-    while True:
-        line = proc.stderr.readline()
-        if line==b'': return
-        print("[ERR]",line)
-        sys.stdout.flush();
+    def _open_io(self, index, name, cmdHeader, enumerator):
+        if index is None and name is None: raise(ValueError)
+        elif index is not None and name is not None: raise(ValueError)
+        elif name:
+            devNames = enumerator()
+            index = devNames.index(name)
+        wr = self.proc.stdin.write
+        wr(cmdHeader)
+        wr(b'%X' % index)
+        wr(self.terminator)
+        self.proc.stdin.flush()
+        self.sem_1.acquire()
+        hexstr = self.gResult;
+        self.sem_2.release()
+        handle = int_from_hex3(hexstr[0],hexstr[1],hexstr[2])
+        print(handle)
+        return handle
 
-def _prepare():
-    global proc
-    global crlf, terminator
-    global thread_monitor_stdout, thread_monitor_stderr
-    global sem_1, sem_2
-    global callback_dic
-    proc = subprocess.Popen('a:/dropbox/home/git/intermidiator/intermidiator.exe',
-                            stdin=subprocess.PIPE,
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE)
-    s_ready = proc.stdout.readline();
-    if s_ready == b'READY\r\n' : crlf, terminator = True, b'\r\n'
-    elif s_ready == b'READY\n' : crlf, terminator = False, b'\n'
-    else:
-        print('ERROR!')
-        return False
+    def open_input(self, index=None, name=None):
+        return self._open_io(index,name,b'OPEN INPUT ',self.enum_input)
 
-    thread_monitor_stdout = threading.Thread(target=monitor_stdout)
-    thread_monitor_stderr = threading.Thread(target=monitor_stderr)
-    sem_1 = threading.Semaphore(0)
-    sem_2 = threading.Semaphore(0)
-    callback_dic = {}
-    return True
+    def open_output(self, index=None, name=None):
+        return self._open_io(index,name,b'OPEN OUTPUT ',self.enum_output)
 
-def _start():
-    thread_monitor_stdout.start()
-    thread_monitor_stderr.start()
+    def listen(self, dev):
+        self.proc.stdin.write(b'LISTEN ')
+        self.proc.stdin.write(b'%X' % dev)
+        self.proc.stdin.write(self.terminator)
+        self.proc.stdin.flush()
 
-def start_process():
-    _prepare()
-    _start()
+    def send(self, dev, msg):
+        if isinstance(msg,str): msg = msg.encode()
+        elif not isinstance(msg, bytes): pass # error
+        self.proc.stdin.write(b'SEND ')
+        self.proc.stdin.write(b'%X ' % dev)
 
-    
-def _enum_io(cmd):
-    proc.stdin.write(cmd)
-    proc.stdin.write(terminator)
-    proc.stdin.flush()
-    sem_1.acquire()
-    names = gResult;
-    sem_2.release()
-    return names
+        self.proc.stdin.write(msg)
+        self.proc.stdin.write(self.terminator)
+        self.proc.stdin.flush()
 
-def enum_input():
-    return _enum_io(b'LIST INPUT')
-def enum_output():
-    return _enum_io(b'LIST OUTPUT')
+    current_callback_id = 100
+    def generate_callback_id(self):
+        self.current_callback_id += 1
+        return self.current_callback_id
 
-def _open_io(index, name, cmdHeader, enumerator):
-    if index is None and name is None: raise(ValueError)
-    elif index is not None and name is not None: raise(ValueError)
-    elif name:
-        devNames = enumerator();
-        index = devNames.index(name)
-    wr = proc.stdin.write
-    wr(cmdHeader)
-    wr(b'%X' % index)
-    wr(terminator)
-    proc.stdin.flush()
-    sem_1.acquire()
-    hexstr = gResult;
-    sem_2.release()
-    handle = int_from_hex3(hexstr[0],hexstr[1],hexstr[2])
-    print(handle)
-    return handle
-
-def open_input(index=None, name=None):
-    return _open_io(index,name,b'OPEN INPUT ',enum_input)
-
-def open_output(index=None, name=None):
-    return _open_io(index,name,b'OPEN OUTPUT ',enum_output)
-
-def listen(dev):
-    proc.stdin.write(b'LISTEN ')
-    proc.stdin.write(b'%X' % dev)
-    proc.stdin.write(terminator)
-    proc.stdin.flush()
-
-def send(dev, msg):
-    if isinstance(msg,str): msg = msg.encode()
-    elif not isinstance(msg, bytes): pass # error
-    proc.stdin.write(b'SEND ')
-    proc.stdin.write(b'%X ' % dev)
-    
-    proc.stdin.write(msg)
-    proc.stdin.write(terminator)
-    proc.stdin.flush()
-    
-current_callback_id = 100
-def generate_callback_id():
-    global current_callback_id
-    current_callback_id += 1
-    return current_callback_id
-    
-def callback(target, signal_pattern, function):
-    proc.stdin.write(b'CALLBACK ')
-    if target is None or target is '*': proc.stdin.write(b'* ')
-    else: proc.stdin.write(b'%X ' % target)
-    proc.stdin.write(signal_pattern.encode())
-    proc.stdin.write(b' ')
-    id = generate_callback_id()
-    callback_dic[id] = function
-    proc.stdin.write(b'%d' % id)
-    proc.stdin.write(terminator)
-    proc.stdin.flush()
+    def callback(self, target, signal_pattern, function):
+        self.proc.stdin.write(b'CALLBACK ')
+        if target is None or target is '*': self.proc.stdin.write(b'* ')
+        else: self.proc.stdin.write(b'%X ' % target)
+        self.proc.stdin.write(signal_pattern.encode())
+        self.proc.stdin.write(b' ')
+        id = self.generate_callback_id()
+        self.callback_dic[id] = function
+        self.proc.stdin.write(b'%d' % id)
+        self.proc.stdin.write(self.terminator)
+        self.proc.stdin.flush()
 
 
 
-def _terminate():
-    wr = proc.stdin.write
-    wr(b'QUIT')
-    wr(terminator)
-    proc.stdin.flush()
-    thread_monitor_stdout.join()
-    thread_monitor_stderr.join()
+    def _terminate(self):
+        wr = self.proc.stdin.write
+        wr(b'QUIT')
+        wr(self.terminator)
+        self.proc.stdin.flush()
+        self.thread_monitor_stdout.join()
+        self.thread_monitor_stderr.join()
 
 
-def stop_process():
-    _terminate()
+    def stop_process(self):
+        self._terminate()
 
-def unsafeCommunicate(bs):
-    proc.stdin.write(bs);
-    proc.stdin.write(terminator);
-    proc.stdin.flush();
+    def unsafeCommunicate(self, bs):
+        self.proc.stdin.write(bs);
+        self.proc.stdin.write(self.terminator);
+        self.proc.stdin.flush();
 
 ###
 
